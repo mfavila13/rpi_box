301d890b35a17cfec54057e807a11d1e6e876a74
Marcus <mfavila13@gmail.com>
2019-07-10 20:47:49 -0500

    c

--- luma_master/examples/pigpio_dem.py
+++ luma_master/examples/pigpio_dem.py
@@ -1,5 +1,6 @@
 import pigpio
-from shiftregister import ShiftRegister
+from explorehat import plugins
+#from shiftregister import ShiftRegister
 
 pi = pigpio.pi()
 
@@ -8,16 +9,16 @@ b_pin = 6
 clr_pin = 13
 clk_pin = 26
 
-sr = ShiftRegister(pi, a_pin, b_pin, clr_pin, clk_pin)
+sr = plugins.ShiftRegister(pi, a_pin, b_pin, clr_pin, clk_pin)
 
-sr.update(0b10101010) # Register outputs 10101010
+#sr.update(0b10101010) # Register outputs 10101010
 
-sr.clear() # Register outputs 00000000
+#sr.clear() # Register outputs 00000000
 
-sr.toggle(0) # Register outputs 00000001
+sr.toggle_pin(0) # Register outputs 00000001
 
-sr.toggle(7) # Register outputs 10000001
+#sr.toggle(7) # Register outputs 10000001
 
-sr.write(7, 0) # Register outputs 00000001
+#sr.write(7, 0) # Register outputs 00000001
 
-sr.write(4, 1) # Register outputs 00010001
+#sr.write(4, 1) # Register outputs 00010001
