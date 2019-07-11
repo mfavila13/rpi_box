e08d70b4a45cb499de915d4647ff85526fac511b
Marcus <mfavila13@gmail.com>
2019-07-11 00:03:19 -0500

    aaaa

--- luma_master/examples/l_dem.py
+++ luma_master/examples/l_dem.py
@@ -7,7 +7,6 @@ class Shifter():
 	inputB=6
 	clock=13
 	clearPin=26
-	
 
 	def __init__(self):
 		self.setupBoard()
@@ -16,19 +15,20 @@ class Shifter():
 		gpio.output(Shifter.clock,gpio.HIGH)
 		sleep(self.pause)
 		gpio.output(Shifter.clock,gpio.LOW)
-		sleep(self.pause)		
+		sleep(self.pause)
 
 	def setValue(self,value):
-		for i in range(8):
-			bitwise=0x800000>>i
-            print(i)
-            bit=bitwise&value
-			if (bit==0):
-				gpio.output(Shifter.inputB,gpio.LOW)
-			else:
-				gpio.output(Shifter.inputB,gpio.HIGH)
-			Shifter.tick(self)
-            
+		for i in range(24):
+	            bitwise=0x800000>>i
+        	    print(i)
+	            bit=bitwise&value
+		    if (bit==0):
+		        gpio.output(Shifter.inputB,gpio.LOW)
+  	            else:
+		        gpio.output(Shifter.inputB,gpio.HIGH)
+     		    #if i == 6:
+		#	gpio.output(Shifter.inputB.gpio.HIGH)
+		    Shifter.tick(self)
 
 	def clear(self):
 		gpio.output(Shifter.clearPin,gpio.LOW)
@@ -37,8 +37,8 @@ class Shifter():
 
 	def setupBoard(self):
 
-		#gpio.setup(Shifter.inputA,gpio.OUT)
-		#gpio.output(Shifter.inputA,gpio.HIGH)
+		gpio.setup(Shifter.inputA,gpio.OUT)
+		gpio.output(Shifter.inputA,gpio.HIGH)
 
 		gpio.setup(Shifter.inputB,gpio.OUT)
 		gpio.output(Shifter.inputB,gpio.LOW)
@@ -50,7 +50,7 @@ class Shifter():
 		gpio.output(Shifter.clearPin,gpio.HIGH)
 
 def main():
-	pause=0.5
+	pause=1
 	gpio.setmode(gpio.BCM)
 	shifter=Shifter()
 	running=True
@@ -60,12 +60,12 @@ def main():
 			#shifter.setValue(1)
 			#sleep(1)
 			shifter.clear()
-			shifter.setValue(0x0AAAAAA)
+			shifter.setValue(0b000000000011111111111111)
 			sleep(pause)
-			shifter.clear()
-			shifter.setValue(0x0555555)
-			sleep(pause)
-            print("loop")
+			#shifter.clear()
+			#shifter.setValue(0x555555)
+			#sleep(pause)
+	                print("loop")
 	        except KeyboardInterrupt:
         		running=False
 
